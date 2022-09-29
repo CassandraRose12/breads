33d7491f347d6538ab9e5897810acf1a180ae149
Cassandra Collins <103472275+CassandraRose12@users.noreply.github.com>
2022-09-29 11:19:00 -0400

    sending index and show data

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -1,20 +1,19 @@
 const React = require('react')
 
 function Default(html) {
-  return (
-    <html>
+    return (
+      <html>
       <head>
-        <title>Default</title>
+        <title>{html.title || 'Default'}</title>
       </head>
       <body>
         <h1>HTML Rendered!</h1>
-        <div className="container">
-            {html.children}
-        </div>
+        {html.children}
       </body>
-    </html>
-  )
-}
+      </html>
+    )
+  }
+  
 
 module.exports = Default
 
