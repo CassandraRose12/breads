78f1b3f0a115157d7ffb27fdd46185eaae41958c
Cassandra Collins <103472275+CassandraRose12@users.noreply.github.com>
2022-10-04 12:28:33 -0400

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -18,6 +18,14 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+//Edit
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
 // Show
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread [req.params.arrayIndex]){
@@ -43,6 +51,18 @@ breads.get('/:arrayIndex', (req, res) => {
     Bread.push(req.body)
     res.redirect('/breads')
   })
+
+  //Update
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
 
   //Delete
   breads.delete('/:indexArray', (req, res) => {
