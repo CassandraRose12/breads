33de1a63096d22d976128a492a997557058ac4ab
Cassandra Collins <103472275+CassandraRose12@users.noreply.github.com>
2022-10-04 11:49:48 -0400

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -22,7 +22,8 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread [req.params.arrayIndex]){
     res.render('Show', 
-    {bread: Bread[req.params.arrayIndex]
+    {bread: Bread[req.params.arrayIndex],
+     index: req.params.arrayIndex,
     })
   }else{
     res.render('404')
@@ -41,6 +42,12 @@ breads.get('/:arrayIndex', (req, res) => {
     }
     Bread.push(req.body)
     res.redirect('/breads')
+  })
+
+  //Delete
+  breads.delete('/:indexArray', (req, res) => {
+    Bread.splice(req.params.indexArray, 1)
+    res.status(303).redirect('/breads')
   })
 
 
